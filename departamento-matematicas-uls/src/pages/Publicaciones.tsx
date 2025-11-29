6e7485875257775ac01fc916e4648844cc296194
BenjaUrbin <benjamin.urbina@userena.cl>
2025-11-29 13:09:17 -0300

    Fixeos menores con vercel

--- departamento-matematicas-uls/src/pages/Publicaciones.tsx
+++ departamento-matematicas-uls/src/pages/Publicaciones.tsx
@@ -1,4 +1,4 @@
-import { Container, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { useState } from "react";
 import NoticiasFeed from "../components/NoticiasFeed";
 type Props = {};
